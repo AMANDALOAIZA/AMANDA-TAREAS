a614107a689fc99f3e4bb4cdb3d0cb4ae7c5b701
AMANDALOAIZA <ing.amanda.loaiza@gmail.com>
2025-09-06 16:55:22 -0400

    Add files via upload

---
+++ ejercicio.py
@@ -0,0 +1,15 @@
+print("Determinar si la persona puede votar y si esta inscrita en el cne")
+print("Suficiente edad para Votar y si esta inscrito en el Cne")
+
+edad= int(input("ingrese su edad"))
+
+inscrito= input ("¿Estas inscrito en el Cne?( SI/NO )").lower()
+
+
+if edad >=18 and inscrito == "si":
+ print ("Usted tiene la edad suficiente para votar y si esta inscrito en el cne")
+
+else:
+ print ("USTED NO CUMPLE CON LA EDAD SUFICIENTE")
+ 18
+ 
---
+++ test.py
@@ -0,0 +1,2 @@
+print(" Esta es la historia de un cerdito que queria una aventura Por eso sale en busca de ella.  Se encuentra dos caminos.")
+opcion1 = input("¿Cuál camino debe escoger")

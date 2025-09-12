7718583f68d50682735f6e7f30fd677b95c8d9c2
AMANDALOAIZA <ing.amanda.loaiza@gmail.com>
2025-09-12 17:09:24 -0400

    Add files via upload

---
+++ diccionario.py
@@ -0,0 +1,9 @@
+mi_diccionario = { 
+    "nombre" : " Amanda", 
+    "apellido" : " Loaiza", 
+    "color favorito" : "Negro",
+
+}
+
+for clave, valor in mi_diccionario.items():
+    print(f"\nClave: {clave}, valor: {valor}")

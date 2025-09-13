c554b06ad67e4c4a13a84e1e19868b8688f5766a
AMANDALOAIZA <ing.amanda.loaiza@gmail.com>
2025-09-13 12:16:26 -0400

    Add files via upload

---
+++ ejerc3.py
@@ -0,0 +1,5 @@
+# Concatenacion de cadenas
+nombre = input ("Introduzca su nombre: ")
+apellido = input ("Introduzca su apellido: ")
+nomb_apell = nombre + " " + apellido
+print (" Su nombre completo es :", nomb_apell)

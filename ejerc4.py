3b14f351f677f9300ed00c6bc194a7163131eb19
AMANDALOAIZA <ing.amanda.loaiza@gmail.com>
2025-09-13 12:54:59 -0400

    Add files via upload

---
+++ ejerc4.py
@@ -0,0 +1,10 @@
+# Verificador de Número Par o Impar
+
+num1 = int (input ("ingrese un numero: "))
+if num1 % 2 == 0 :
+   print ("Su numero es PAR")
+
+else :
+    print ("Su numero es Impar")
+    
+    

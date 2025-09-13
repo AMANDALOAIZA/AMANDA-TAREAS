5c192b73e07d0242e6fe4be1f6a324b7e0539901
AMANDALOAIZA <ing.amanda.loaiza@gmail.com>
2025-09-13 11:57:49 -0400

    Add files via upload

---
+++ ejerc2.py
@@ -0,0 +1,4 @@
+# Conversor de temperatura celsius a Farenheit
+Celsius = float (input ("Ingrese los grados Celsius: "))
+Farenheit = (Celsius * 9/5) + 32
+print ("Tu resultado es ", Farenheit, "F")

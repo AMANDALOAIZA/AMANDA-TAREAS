cac19f7eadebe948e41be114e244e84f3d62ab78
AMANDALOAIZA <ing.amanda.loaiza@gmail.com>
2025-09-13 13:23:46 -0400

    Add files via upload

---
+++ ejerc6.py
@@ -0,0 +1,9 @@
+#Comparador de Números
+num1 = int (input ("Ingrese primer numero: " ))
+num2 = int (input ("Ingrese segundo numero: "))
+if num1 > num2:
+    print ("Su numero", num1, "es mayor que: ", num2)
+elif num1 < num2:
+    print ("Su numero",num1, "es menor que:", num2)
+else:
+    print ("Su numero",num1, "es igual que:", num2)
